a365d5c79a2bd6ade98d79544476fe2ca6329aee
Systembetreuung Schule Bayern <systembetreuung@schule-bayern.de>
2026-07-06 18:01:46 +0200

    Allgemeine Infoseite als Startseite hinzugefuegt

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,5 +3,5 @@ import {Redirect} from '@docusaurus/router';
 
 export default function Home() {
   // Leitet Besucher jetzt direkt auf die allgemeine Infoseite weiter
-  return <Redirect to="/admin-zimmer/docs/0-0-start" />;
+  return <Redirect to="/admin-zimmer/docs/0-0-startseite" />;
 }
